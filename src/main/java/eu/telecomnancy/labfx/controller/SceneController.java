3f62e673644f77a333364876f0c685fc06a73007
Lili <lili.schmidlin@telecomnancy.eu>
2024-01-08 12:50:31 +0100

    début scene switch

---
+++ src/main/java/eu/telecomnancy/labfx/controller/SceneController.java
@@ -0,0 +1,24 @@
+package eu.telecomnancy.labfx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goToAccueil(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("accueil.fxml"));
+        root = loader.load();
+        stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
